03238e33ed2f0af6185da82b1d1636eff9a3337e
Davyd1234 <156998456+Davyd1234@users.noreply.github.com>
2024-01-18 15:45:07 +0800

    Add files via upload

---
+++ ConsoleApplication18/ConsoleApplication18/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет!");
+            Console.WriteLine("Hello!");
+            Console.ReadLine();
+
+        }
+    }
+}
